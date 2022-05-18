36b6f241beea4482aace1983b38a11bdce081583
Arsham Shirvani <arshamshirvani@gmail.com>
2022-05-18 08:01:49 +0100

    fix(autoload): fix autoload setup

--- arsham/autoload.lua
+++ arsham/autoload.lua
@@ -2,7 +2,7 @@ local naughty = require("naughty")
 local awful = require("awful")
 
 local function run_once(item)
-  local match = item.match
+  local cmd = item.cmd
   local name = item.name
   local kill = item.kill
   if kill then
@@ -22,7 +22,7 @@ local function run_once(item)
   end
 
   if item.sleep then
-    cmd_str = cmd_str .. string.format("(sleep %d ; %s)", item.sleep, match)
+    cmd_str = cmd_str .. string.format("(sleep %d ; %s)", item.sleep, cmd)
 
     awful.spawn.easy_async_with_shell(cmd_str, function(_, err)
       if err and err ~= "" then
@@ -36,7 +36,7 @@ local function run_once(item)
     return
   end
 
-  local _, err = awful.spawn.with_shell(cmd_str .. match)
+  local _, err = awful.spawn.with_shell(cmd_str .. cmd)
   if err and err ~= "" then
     naughty.notify({
       preset = naughty.config.presets.critical,
@@ -49,30 +49,27 @@ end
 -- run xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
 -- run xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 -- autorandr horizontal
-run_once({ name = "unclutter", match = "unclutter -root" })
-run_once({ name = "setxkbmap", match = "setxkbmap -option caps:escape" })
-run_once({ name = "nm-applet", match = "nm-applet" })
-run_once({ name = "caffeine", kill = "caffeine", match = "caffeine" })
-run_once({ name = "xfce4-power-manager", match = "xfce4-power-manager" })
-run_once({ name = "blueberry-tray", match = "blueberry-tray" })
+run_once({ name = "unclutter", cmd = "unclutter -root" })
+run_once({ name = "setxkbmap", cmd = "setxkbmap -option caps:escape" })
+run_once({ name = "nm-applet", cmd = "nm-applet" })
+run_once({ name = "caffeine", kill = "caffeine", cmd = "caffeine" })
+run_once({ name = "xfce4-power-manager", cmd = "xfce4-power-manager" })
 run_once({
   name = "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1",
-  match = "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1",
+  cmd = "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1",
 })
-run_once({ name = "numlockx", match = "numlockx on" })
-run_once({ name = "volumeicon", match = "volumeicon" })
-run_once({ name = "redshift", match = "redshift" })
+run_once({ name = "numlockx", cmd = "numlockx on" })
+run_once({ name = "volumeicon", cmd = "volumeicon" })
+run_once({ name = "redshift-gtk", cmd = "redshift-gtk", kill = "redshift-gtk" })
 run_once({
   name = "variety",
-  match = "variety --profile /home/arsham/.config/variety/",
+  cmd = "variety --profile /home/arsham/.config/variety/",
   kill = "variety",
   sleep = 1,
 })
-run_once({ name = "dropbox", match = "dropbox", sleep = 3 })
-run_once({ name = "unclutter", match = "unclutter -root" })
-run_once({ name = "parcellite", match = "parcellite", sleep = 3 })
-run_once({ name = "syndaemon", match = "syndaemon -i 0.5 -t -K -R" })
+run_once({ name = "parcellite", cmd = "parcellite", sleep = 3 })
+run_once({ name = "syndaemon", cmd = "syndaemon -i 0.5 -t -K -R" })
 run_once({
   name = "picom",
-  match = "picom -b --experimental-backends --backend glx --config  $HOME/.config/awesome/picom.conf",
+  cmd = "picom -b --experimental-backends --backend glx --config  $HOME/.config/awesome/picom.conf",
 })
