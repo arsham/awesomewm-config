11bc3100990da1f5b1b23ce39f6c847f7dffa340
Arsham Shirvani <arshamshirvani@gmail.com>
2022-05-19 17:58:54 +0100

    ref(theme): rearrange icon locations

--- theme/theme.lua
+++ theme/theme.lua
@@ -1,5 +1,4 @@
 local dpi = require("beautiful.xresources").apply_dpi
-local beautiful = require("beautiful")
 
 local theme = {}
 
@@ -10,13 +9,12 @@ local function icon(name)
 end
 
 theme.default_bg = "#232627"
--- theme.default_bg = "#23262744"
 theme.bg_systray = theme.default_bg
 
 -- Global {{{
 theme.useless_gap = 5
 theme.awesome_icon = icon("awesome.png")
--- awesome.set_preferred_icon_size(48)
+awesome.set_preferred_icon_size(48)
 theme.wallpaper = theme.dir .. "/wallpaper.jpg"
 theme.font_name = "Ubuntu R "
 theme.font = theme.font_name .. "13"
@@ -53,30 +51,30 @@ theme.menu_width = dpi(260)
 theme.menu_submenu_icon = icon("submenu.png")
 --}}}
 -- Layout {{{
-theme.layout_tile = icon("tile.png")
-theme.layout_tileleft = icon("tileleft.png")
-theme.layout_tilebottom = icon("tilebottom.png")
-theme.layout_tiletop = icon("tiletop.png")
-theme.layout_fairv = icon("fairv.png")
-theme.layout_fairh = icon("fairh.png")
-theme.layout_spiral = icon("spiral.png")
-theme.layout_dwindle = icon("dwindle.png")
-theme.layout_max = icon("max.png")
-theme.layout_fullscreen = icon("fullscreen.png")
-theme.layout_magnifier = icon("magnifier.png")
-theme.layout_floating = icon("floating.png")
-theme.layout_cascade = icon("cascadew.png")
-theme.layout_cascadetile = icon("cascadetilew.png")
-theme.layout_centerwork = icon("centerworkw.png")
-theme.layout_centerworkh = icon("centerworkhw.png")
-theme.layout_termfair = icon("termfairw.png")
-theme.layout_centerfair = icon("centerfairw.png")
+theme.layout_tile = icon("layout/tile.png")
+theme.layout_tileleft = icon("layout/tileleft.png")
+theme.layout_tilebottom = icon("layout/tilebottom.png")
+theme.layout_tiletop = icon("layout/tiletop.png")
+theme.layout_fairv = icon("layout/fairv.png")
+theme.layout_fairh = icon("layout/fairh.png")
+theme.layout_spiral = icon("layout/spiral.png")
+theme.layout_dwindle = icon("layout/dwindle.png")
+theme.layout_max = icon("layout/max.png")
+theme.layout_fullscreen = icon("layout/fullscreen.png")
+theme.layout_magnifier = icon("layout/magnifier.png")
+theme.layout_floating = icon("layout/floating.png")
+theme.layout_cascade = icon("layout/cascadew.png")
+theme.layout_cascadetile = icon("layout/cascadetilew.png")
+theme.layout_centerwork = icon("layout/centerworkw.png")
+theme.layout_centerworkh = icon("layout/centerworkhw.png")
+theme.layout_termfair = icon("layout/termfairw.png")
+theme.layout_centerfair = icon("layout/centerfairw.png")
 --}}}
 -- Widget {{{
 theme.widget_ac = icon("ac.png")
-theme.widget_battery = icon("battery.png")
-theme.widget_battery_low = icon("battery_low.png")
-theme.widget_battery_empty = icon("battery_empty.png")
+theme.widget_battery = icon("battery/battery.png")
+theme.widget_battery_low = icon("battery/battery_low.png")
+theme.widget_battery_empty = icon("battery/battery_empty.png")
 theme.widget_mem = icon("mem.png")
 theme.widget_cpu = icon("cpu.png")
 theme.widget_temp = icon("temp.png")
@@ -86,10 +84,10 @@ theme.widget_music = icon("note.png")
 theme.widget_music_on = icon("note.png")
 theme.widget_music_pause = icon("pause.png")
 theme.widget_music_stop = icon("stop.png")
-theme.widget_vol = icon("vol.png")
-theme.widget_vol_low = icon("vol_low.png")
-theme.widget_vol_no = icon("vol_no.png")
-theme.widget_vol_mute = icon("vol_mute.png")
+theme.widget_vol = icon("volume/vol.png")
+theme.widget_vol_low = icon("volume/vol_low.png")
+theme.widget_vol_no = icon("volume/vol_no.png")
+theme.widget_vol_mute = icon("volume/vol_mute.png")
 theme.widget_mail = icon("mail.png")
 theme.widget_mail_on = icon("mail_on.png")
 theme.widget_task = icon("task.png")
